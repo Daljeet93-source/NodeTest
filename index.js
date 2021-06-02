b3d8333113eec757d7f2d67416fc62f205465384
Daljeet93-source <dsdjsingh65@gmail.com>
2021-06-02 13:04:40 +0530

    Express Started

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const express = require('express')
+
+const app = express();
+
+
+app.get('/',(req, res) => {
+    res.send("Hello World")
+})
+
+app.post('/add',(req,res) => {
+    res.send("Saved succesfully")
+})
+
+app.listen(5000,() => {
+    console.log("Connected")
+})
