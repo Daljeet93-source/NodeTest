11025b60182a3b8b02d1d308c5543cee2a99b44e
Daljeet93-source <dsdjsingh65@gmail.com>
2021-06-04 13:04:09 +0530

    send html file

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const path = require("path")
 
 const app = express();
 
 
 app.get('/',(req, res) => {
-    res.send("Hello World")
+    // res.send("<h1>Hello World</h1>")
+    res.sendFile(path.join(__dirname,"index.html"))
 })
 
-app.post('/add',(req,res) => {
-    res.send("Saved succesfully")
+app.get('/add',(req,res) => {
+    // res.send("Saved succesfully")
+    res.sendFile(path.join(__dirname,'add.html'))
+})
+
+app.get('/json',(req,res) => {
+    res.json({Name:"Aman",Age:12,Marks:[23,65,98,78]})
 })
 
 app.listen(5000,() => {
